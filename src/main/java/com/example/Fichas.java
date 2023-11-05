c50a9ce326335e2fe0166a4db302f4586bbe5a84
Raul <rfernr08@estudiantes.unileon.es>
2023-11-05 17:04:52 +0100

    inicio2

--- src/main/java/com/example/Fichas.java
+++ src/main/java/com/example/Fichas.java
@@ -1,7 +1,18 @@
 package com.example;
+import java.util.Scanner;
 
 public class Fichas {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        String juego = input.nextLine();
+        int numJuegos = juego.charAt(0);
+        int f = 0, c = 0;
+        while(juego.charAt(0) != null){
+            c++;
+        }
+    }
+
+    public boolean checkBoards(){
+        
     }
 }
